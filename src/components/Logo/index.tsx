2089dfd0498382b8db04adab92ea244d88ce02be
GBrogio <guilhermebrogio.ps@email.com>
2022-07-13 17:10:33 -0300

    chore: create Logo Mocked component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import { ParagraphCSS } from '@stylesComponents/Texts';
+import { ContainerCSS } from './styles';
+
+export const Logo = () => {
+  return (
+    <ContainerCSS>
+      <ParagraphCSS bold>LOGO</ParagraphCSS>
+    </ContainerCSS>
+  );
+};
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ContainerCSS = styled.div`
+  font-size: 3rem;
+
+  > p {
+    margin-bottom: 0;
+  }
+`;
