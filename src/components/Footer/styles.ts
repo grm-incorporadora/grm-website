d98f23dcb1fd6c88d79abb2d4322a13de5537081
GBrogio <guilhermebrogio.ps@email.com>
2022-07-13 17:09:03 -0300

    chore: create Footer component

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ContainerCSS = styled.footer`
+  text-transform: uppercase;
+
+  width: 100%;
+
+  color: rgb(var(--color-text900));
+  background-color: rgb(var(--color-background100));
+
+  > .bottom {
+    text-align: center;
+    padding: 1.5rem 0;
+
+    color: rgb(var(--color-text100));
+    background-color: rgb(var(--color-background900));
+  }
+`;
