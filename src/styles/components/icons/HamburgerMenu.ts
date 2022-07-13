fc5c936351d94797ebf717d95629d2cbfa1d4bb9
GBrogio <guilhermebrogio.ps@email.com>
2022-07-13 17:27:40 -0300

    chore: create HamburgeMenu styled icon

---
+++ src/styles/components/icons/HamburgerMenu.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Menu } from '@styled-icons/boxicons-regular/Menu';
+
+export const HamburgerMenuIcon = styled(Menu)`
+  width: 2rem;
+  height: 2rem;
+  color: inherit;
+`;
