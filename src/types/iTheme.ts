f6bb9920c5443112cd6dbddeb0a1664b2ae2b5a3
GBrogio <guilhermebrogio.ps@email.com>
2022-07-13 17:32:48 -0300

    chore: create theme interface

---
+++ src/types/iTheme.ts
@@ -0,0 +1,34 @@
+export interface iSharedThemeOptions {
+  maxWidth: string;
+
+  header: {
+    mobileHeight: string;
+    desktopHeight: string;
+  };
+  fonts: {
+    size: string;
+    primary: string;
+  };
+  mediaQueries: {
+    maxMobile: string;
+    maxTablet: string;
+    minTablet: string;
+    minMobile: string;
+  };
+}
+
+export interface iTheme extends iSharedThemeOptions {
+  name: 'default';
+
+  colors: {
+    primary: string;
+    text100: string;
+    text300: string;
+    text600: string;
+    text900: string;
+    background100: string;
+    background300: string;
+    background600: string;
+    background900: string;
+  };
+}
